b80b1b73cc6645e18beb3bb86eb67877a641c6a4
hublearner184 <1819101984@qq.com>
2026-01-16 18:36:30 +0800

    Update main.cpp
    理解指针及双指针。

--- test_ptr/test_ptr/src/main.cpp
+++ test_ptr/test_ptr/src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main()
 {
-	int var = 8;
-	int* ptr = &var;
-	*ptr = 10;//Ö¸ÕëµÄÄæÓÃ
+	char* buffer = new char[8];
+	memset(buffer, 0, 8);
+	char** ptr = &buffer;
 	std::cin.get();
 }
