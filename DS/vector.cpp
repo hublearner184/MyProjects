f960b2b988d6032df6c366e8087e2a73f05235d7
hublearner184 <1819101984@qq.com>
2026-01-29 23:52:18 +0800

    1

--- DS/vector.cpp
+++ DS/vector.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+vector<int> nums;
 
 int main()
 {
-    int arr[20];
-    memset(arr, 0, sizeof(arr));
+    int n = 10;
 
-    for (int i = 0; i < 10; i++)
-    {
-        arr[i] = i;
-    }
-    
-    for(int i = 2; i < 10; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
+    vector<int> nums(n);
+   
+    cout << nums.empty() << endl;
 
-    for(int i = 10; i > 2; i--)
+    cout << nums.size() << endl;
+
+    nums.push_back(20);//在末尾添加元素
+
+    cout << nums.size() << endl;
+
+    cout << nums.back() << endl;//返回最后一个元素
+
+    nums.pop_back();//删除最后一个元素
+
+    nums[0] = 11;
+
+    cout << nums[0] << endl;
+
+    nums.insert(nums.begin() + 3,99);//在指定位置插入元素
+
+    nums.erase(nums.begin() + 2);//删除指定位置元素
+
+    swap(nums[0],nums[1]);//交换两个元素
+
+    for(int i = 0;i < nums.size(); i++)
     {
-        arr[i] = arr[i - 1];
+        cout << nums[i] << " ";
     }
+    cout << endl;
+
 
 }
